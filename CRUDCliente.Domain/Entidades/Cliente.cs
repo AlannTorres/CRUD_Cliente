a47e2a010bf7bc19cd945e7db184fa9f39f7c6a9
AlannTorres <alanvictor.torres@gmail.com>
2023-11-07 10:39:16 -0300

    feat: Criação da entidade cliente

---
+++ CRUDCliente.Domain/Entidades/Cliente.cs
@@ -0,0 +1,39 @@
+﻿using CRUDCliente.Domain.Validation;
+
+namespace CRUDCliente.Domain.Entidades;
+
+public sealed class Cliente
+{
+    public int Id { get; private set; }
+    public string Nome { get; private set; }
+    public string Cpf { get; private set; }
+    public string Telefone { get; private set; }
+
+    public Cliente(int id, string nome, string cpf, string telefone)
+    {
+        DomainExceptionValidation.When(id < 0, "Invalid Id value");
+        Id = id;
+        ValidationDomain(nome, cpf, telefone);
+    }
+
+    public Cliente(string nome, string cpf, string telefone)
+    {
+        ValidationDomain(nome, cpf, telefone);
+    }
+
+    public void ValidationDomain(string nome, string cpf, string telefone)
+    {
+        DomainExceptionValidation.When(string.IsNullOrEmpty(nome), "Invalid name. Name is required");
+        DomainExceptionValidation.When(nome.Length < 3,"Invalid name, too short, minimum 3 characters");
+
+        DomainExceptionValidation.When(string.IsNullOrEmpty(cpf), "Invalid cpf. Cpf is required");
+        DomainExceptionValidation.When(cpf.Length != 11, "Invalid cpf, required 11 characters");
+
+        DomainExceptionValidation.When(string.IsNullOrEmpty(telefone), "Invalid telefone. Telefone is required");
+        DomainExceptionValidation.When(telefone.Length != 9, "Invalid telefone,required 9 characters");
+
+        Nome = nome;
+        Cpf = cpf;
+        Telefone = telefone;
+    }
+}
