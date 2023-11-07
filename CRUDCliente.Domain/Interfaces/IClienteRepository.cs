a4595ef9e165a15254cbaa57cbd2312b3387d4f3
AlannTorres <alanvictor.torres@gmail.com>
2023-11-07 10:39:58 -0300

    feat: Criação interface cliente

---
+++ CRUDCliente.Domain/Interfaces/IClienteRepository.cs
@@ -0,0 +1,12 @@
+﻿using CRUDCliente.Domain.Entidades;
+
+namespace CRUDCliente.Domain.Interfaces;
+
+public interface IClienteRepository
+{
+    Task<IEnumerable<Cliente>> GetAllAsync();
+    Task<Cliente> GetByIdAsync(int id);
+    Task<Cliente> CreateAsync(Cliente cliente);
+    Task<Cliente> UpdateAsync(Cliente cliente);
+    Task<Cliente> RemoveAsync(Cliente cliente);
+}
