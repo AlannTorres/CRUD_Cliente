4d25b5f0117b56f81680a40b85be87ad4464fb5b
AlannTorres <alanvictor.torres@gmail.com>
2023-11-07 10:46:25 -0300

    feat: Criação da interface de services

---
+++ CRUDCliente.Application/Interfaces/IClienteService.cs
@@ -0,0 +1,12 @@
+﻿using CRUDCliente.Application.DTOs;
+
+namespace CRUDCliente.Application.Interfaces;
+
+public interface IClienteService
+{
+    Task<IEnumerable<ClienteDTO>> GetClientes();
+    Task<ClienteDTO> GetClienteById(int idCliente);
+    Task AdicionarCliente(ClienteDTO clienteDTO);
+    Task EditarCliente(ClienteDTO clienteDTO);
+    Task DeleteCliente(int idCliente);
+}
