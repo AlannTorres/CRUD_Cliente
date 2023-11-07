5e197e14a26117cadaa2c1e556e7b61c8f1d608a
AlannTorres <alanvictor.torres@gmail.com>
2023-11-07 10:46:59 -0300

    feat: Criação da classe de mapping do automapper

---
+++ CRUDCliente.Application/Mapping/DomainToDTOMapping.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using CRUDCliente.Application.DTOs;
+using CRUDCliente.Domain.Entidades;
+
+namespace CRUDCliente.Application.Mapping;
+
+public class DomainToDTOMapping : Profile
+{
+    public DomainToDTOMapping()
+    {
+        CreateMap<Cliente, ClienteDTO>().ReverseMap();   
+    }
+}
