3c2beda9cbeb40be08c335469d5fa84fc1d3018b
AlannTorres <alanvictor.torres@gmail.com>
2023-11-07 10:43:30 -0300

    feat: Criação da camada de teste API

---
+++ CRUDCliente.API.Tests/CleinteDTOTests.cs
@@ -0,0 +1,152 @@
+﻿using CRUDCliente.Application.DTOs;
+using System.ComponentModel.DataAnnotations;
+
+namespace CRUDCliente.API.Tests;
+
+public class CleinteDTOTests
+{
+    [Fact(DisplayName = "Declaração sem o atributo nome")]
+    public void Nome_RequiredValidation_Success()
+    {
+        // Arranjo
+        var cliente = new ClienteDTO();
+
+        // Ação
+        var resultadosDeValidacao = ValidateModel(cliente);
+
+        // Afirmação
+        var resultadoDeValidacaoNome = resultadosDeValidacao
+            .Find(result => result.MemberNames.Contains(nameof(ClienteDTO.Nome)));
+        Assert.NotNull(resultadoDeValidacaoNome);
+        Assert.Contains("The Nome is Required", resultadoDeValidacaoNome.ErrorMessage);
+    }
+
+    [Fact(DisplayName = "Declaração com atributo nome menor que 3 caracteres")]
+    public void Nome_MinLengthValidation_Success()
+    {
+        // Arranjo
+        var cliente = new ClienteDTO
+        {
+            Nome = "A"
+        };
+
+        // Ação
+        var resultadosDeValidacao = ValidateModel(cliente);
+
+        // Afirmação
+        var resultadoDeValidacaoNome = resultadosDeValidacao
+            .Find(result => result.MemberNames.Contains(nameof(ClienteDTO.Nome)));
+        Assert.NotNull(resultadoDeValidacaoNome);
+        Assert.Contains("The field Nome must be a string or array type with a minimum length of '3'.", resultadoDeValidacaoNome.ErrorMessage);
+    }
+
+    [Fact(DisplayName = "Declaração com atributo nome maior que 100 caracteres")]
+    public void Nome_MaxLengthValidation_Success()
+    {
+        var cliente = new ClienteDTO
+        {
+            Nome = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec id est tristique, semper augue quis, lobortis leo. Aliquam sed viverra nisl, quis accumsan erat. Curabitur posuere ligula vitae sapien euismod, sed mattis dui feugiat. Etiam et placerat sapien. In vitae ligula pretium, mattis augue in, interdum augue."
+        };
+
+        var resultadosDeValidacao = ValidateModel(cliente);
+
+        var resultadoDeValidacaoNome = resultadosDeValidacao
+            .Find(result => result.MemberNames.Contains(nameof(ClienteDTO.Nome)));
+        Assert.NotNull(resultadoDeValidacaoNome);
+        Assert.Contains("The field Nome must be a string or array type with a maximum length of '100'.", resultadoDeValidacaoNome.ErrorMessage);
+    }
+
+    [Fact(DisplayName = "Declaração sem o atributo cpf")]
+    public void Cpf_RequiredValidation_Success()
+    {
+        // Arranjo
+        var cliente = new ClienteDTO();
+
+        // Ação
+        var resultadosDeValidacao = ValidateModel(cliente);
+
+        // Afirmação
+        var resultadoDeValidacaoCpf = resultadosDeValidacao
+            .Find(result => result.MemberNames.Contains(nameof(ClienteDTO.Cpf)));
+        Assert.NotNull(resultadoDeValidacaoCpf);
+        Assert.Contains("The Cpf is Required", resultadoDeValidacaoCpf.ErrorMessage);
+    }
+
+    [Fact(DisplayName = "Declaração com atributo cpf menor que 11 caracteres")]
+    public void Cpf_MinLengthValidation_Success()
+    {
+        // Arranjo
+        var cliente = new ClienteDTO
+        {
+            Cpf = "5421"
+        };
+
+        // Ação
+        var resultadosDeValidacao = ValidateModel(cliente);
+
+        // Afirmação
+        var resultadoDeValidacaoCpf = resultadosDeValidacao
+            .Find(result => result.MemberNames.Contains(nameof(ClienteDTO.Cpf)));
+        Assert.NotNull(resultadoDeValidacaoCpf);
+        Assert.Contains("The field Cpf must be a string or array type with a minimum length of '11'.", resultadoDeValidacaoCpf.ErrorMessage);
+    }
+
+    [Fact(DisplayName = "Declaração com atributo nome maior que 11 caracteres")]
+    public void Cpf_MaxLengthValidation_Success()
+    {
+        var cliente = new ClienteDTO
+        {
+            Cpf = "054221635558566654454336"
+        };
+
+        var resultadosDeValidacao = ValidateModel(cliente);
+
+        var resultadoDeValidacaoCpf = resultadosDeValidacao
+            .Find(result => result.MemberNames.Contains(nameof(ClienteDTO.Cpf)));
+        Assert.NotNull(resultadoDeValidacaoCpf);
+        Assert.Contains("The field Cpf must be a string or array type with a maximum length of '11'.", resultadoDeValidacaoCpf.ErrorMessage);
+    }
+
+    [Fact(DisplayName = "Declaração sem o atributo telefone")]
+    public void Telefone_RequiredValidation_Success()
+    {
+        // Arranjo
+        var cliente = new ClienteDTO();
+
+        // Ação
+        var resultadosDeValidacao = ValidateModel(cliente);
+
+        // Afirmação
+        var resultadoDeValidacaoTelefone = resultadosDeValidacao
+            .Find(result => result.MemberNames.Contains(nameof(ClienteDTO.Telefone)));
+        Assert.NotNull(resultadoDeValidacaoTelefone);
+        Assert.Contains("The Telefone is Required", resultadoDeValidacaoTelefone.ErrorMessage);
+    }
+
+    [Fact(DisplayName = "Declaração com atributo telefone menor que 9 caracteres")]
+    public void Telefone_MinLengthValidation_Success()
+    {
+        // Arranjo
+        var cliente = new ClienteDTO
+        {
+            Telefone = "5421"
+        };
+
+        // Ação
+        var resultadosDeValidacao = ValidateModel(cliente);
+
+        // Afirmação
+        var resultadoDeValidacaoTelefone = resultadosDeValidacao
+            .Find(result => result.MemberNames.Contains(nameof(ClienteDTO.Telefone)));
+        Assert.NotNull(resultadoDeValidacaoTelefone);
+        Assert.Contains("The field Telefone must be a string or array type with a minimum length of '9'.", resultadoDeValidacaoTelefone.ErrorMessage);
+    }
+
+    private List<ValidationResult> ValidateModel(object model)
+    {
+        var contexto = new ValidationContext(model, null, null);
+        var resultados = new List<ValidationResult>();
+        Validator.TryValidateObject(model, contexto, resultados, true);
+        return resultados;
+    }
+}
---
+++ CRUDCliente.API.Tests/ControllerClienteTests.cs
@@ -0,0 +1,142 @@
+using Microsoft.AspNetCore.Mvc;
+using CRUDCliente.API.Controller;
+using CRUDCliente.Application.DTOs;
+using CRUDCliente.Application.Interfaces;
+using Moq;
+
+namespace CRUDCliente.API.Tests;
+
+public class ControllerClienteTests
+{
+    [Fact(DisplayName = "Buscar Lista de Clientes")]
+    public async Task GetClientes_ReturnsListOfClientes()
+    {
+        // Arrange
+        var clienteServiceMock = new Mock<IClienteService>();
+        var controller = new ClienteController(clienteServiceMock.Object);
+
+        var expectedClientes = new List<ClienteDTO>();
+        expectedClientes.Add(new ClienteDTO {
+            Id = 1,
+            Nome = "Alan",
+            Cpf = "022230589128",
+            Telefone = "8999355087"
+        });
+        expectedClientes.Add(new ClienteDTO
+        {
+            Id = 3,
+            Nome = "Maria",
+            Cpf = "022230589128",
+            Telefone = "8999355087"
+        });
+
+        clienteServiceMock.Setup(service 
+            => service.GetClientes()).ReturnsAsync(expectedClientes);
+
+        // Act
+        var result = await controller.GetClientes();
+
+        // Assert
+        var actionResult = Assert.IsType<ActionResult<IEnumerable<ClienteDTO>>>(result);
+        var okResult = Assert.IsType<OkObjectResult>(actionResult.Result);
+        var model = Assert.IsAssignableFrom<IEnumerable<ClienteDTO>>(okResult.Value);
+        Assert.Same(expectedClientes, model);
+    }
+
+    [Fact(DisplayName = "Buscar Cliente com id valido")]
+    public async Task GetClienteById_WithValidId_ReturnsCliente()
+    {
+        // Arrange
+        var clienteServiceMock = new Mock<IClienteService>();
+        var controller = new ClienteController(clienteServiceMock.Object);
+
+        var expectedCliente = new ClienteDTO()
+        {
+            Id = 1,
+            Nome = "Alan",
+            Cpf = "022230589128",
+            Telefone = "8999355087"
+        }; 
+        clienteServiceMock.Setup(service 
+            => service.GetClienteById(It.IsAny<int>())).ReturnsAsync(expectedCliente);
+
+        // Act
+        var result = await controller.GetClienteById(1);
+
+        // Assert
+        var actionResult = Assert.IsType<ActionResult<ClienteDTO>>(result);
+        var okResult = Assert.IsType<OkObjectResult>(actionResult.Result);
+        var model = Assert.IsType<ClienteDTO>(okResult.Value);
+        Assert.Same(expectedCliente, model);
+    }
+
+    [Fact(DisplayName = "Buscar Cliente com id não existente")]
+    public async Task GetClienteById_WithInvalidId_ReturnsNotFound()
+    {
+        // Arrange
+        var clienteServiceMock = new Mock<IClienteService>();
+        var controller = new ClienteController(clienteServiceMock.Object);
+
+        clienteServiceMock.Setup(service 
+            => service.GetClienteById(It.IsAny<int>())).ReturnsAsync((ClienteDTO)null);
+
+        // Act
+        var result = await controller.GetClienteById(1);
+
+        // Assert
+        var actionResult = Assert.IsType<ActionResult<ClienteDTO>>(result);
+        var notFoundResult = Assert.IsType<NotFoundObjectResult>(actionResult.Result);
+        Assert.Equal("Cliente não encontrado", notFoundResult.Value);
+    }
+
+    [Fact(DisplayName = "Criar Cliente Válido")]
+    public async Task CreateCliente_WithValidData_ReturnsOkResult()
+    {
+        // Arrange
+        var clienteServiceMock = new Mock<IClienteService>();
+        var controller = new ClienteController(clienteServiceMock.Object);
+
+        var clienteDTO = new ClienteDTO
+        {
+            Nome = "João",
+            Cpf = "12345678901",
+            Telefone = "9999999999"
+        };
+
+        // Act
+        var result = await controller.CreateCliente(clienteDTO);
+
+        // Assert
+        var okResult = Assert.IsType<OkObjectResult>(result);
+        var model = Assert.IsType<ClienteDTO>(okResult.Value);
+        Assert.Equal(clienteDTO.Nome, model.Nome);
+        Assert.Equal(clienteDTO.Cpf, model.Cpf);
+        Assert.Equal(clienteDTO.Telefone, model.Telefone);
+    }
+
+    [Fact(DisplayName = "Atualizar Cliente Válido")]
+    public async Task UpdateCliente_WithValidData_ReturnsOkResult()
+    {
+        // Arrange
+        var clienteServiceMock = new Mock<IClienteService>();
+        var controller = new ClienteController(clienteServiceMock.Object);
+
+        var clienteDTO = new ClienteDTO
+        {
+            Id = 1, // ID válido de um cliente existente
+            Nome = "Novo Nome",
+            Cpf = "12345678901",
+            Telefone = "9999999999"
+        };
+
+        // Act
+        var result = await controller.EditCliente(1, clienteDTO);
+
+        // Assert
+        var okResult = Assert.IsType<OkObjectResult>(result);
+        var model = Assert.IsType<ClienteDTO>(okResult.Value);
+        Assert.Equal(clienteDTO.Nome, model.Nome);
+        Assert.Equal(clienteDTO.Cpf, model.Cpf);
+        Assert.Equal(clienteDTO.Telefone, model.Telefone);
+    }
+}
