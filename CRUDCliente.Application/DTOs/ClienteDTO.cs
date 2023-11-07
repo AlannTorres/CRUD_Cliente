149bbd69d37b43c33fb8972e10afe7c666419e51
AlannTorres <alanvictor.torres@gmail.com>
2023-11-07 10:45:58 -0300

    feat Criação dos DTOs

---
+++ CRUDCliente.Application/DTOs/ClienteDTO.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRUDCliente.Application.DTOs;
+
+public class ClienteDTO
+{
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "The Nome is Required")]
+    [MinLength(3)]
+    [MaxLength(100)]
+    public string? Nome { get; set; }
+    
+    [Required(ErrorMessage = "The Cpf is Required")]
+    [MinLength(11)]
+    [MaxLength(11)]
+    public string? Cpf { get; set;}
+
+    [Required(ErrorMessage = "The Telefone is Required")]
+    [MinLength(9)]
+    public string? Telefone { get; set;}
+}
